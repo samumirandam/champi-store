c77a01fba42e0fd32cc1347b16d7879974f90df2
samumirandam <samuelchampi@gmail.com>
2021-03-11 21:19:26 -0500

    Se agregan las rutas y vistas del proyecto base

---
+++ src/routes/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import Home from '../containers/Home';
+import NotFound from '../containers/NotFound';
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route component={NotFound} />
+      </Switch>
+    </BrowserRouter>
+  );
+};
+
+export default App;
